eefe15081280e2309136f6856cfa6739c64d3194
Genevive-Mbesi <genevive.opondo@student.moringaschool.com>
2023-07-28 02:18:32 +0300

    done with assignment

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,21 +1,56 @@
 import React from "react";
 
+import React from "react";
+import React, { useState } from "react";
+
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [formData, setFormData] = useState({ text: "", category: "" });
+  const optionsList = categories.map((category) => (
+    <option key={category} value={category}>
+      {category}
+    </option>
+  ));
+
+  const handleOnChange = (e) => {
+    const newObj = { ...formData, [e.target.name]: e.target.value };
+    setFormData(newObj);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    onTaskFormSubmit(formData);
+  };
+
 function NewTaskForm() {
   return (
     <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleFormSubmit}>
       <label>
         Details
         <input type="text" name="text" />
+        <input
+          type="text"
+          name="text"
+          value={formData.text}
+          onChange={handleOnChange}
+        />
       </label>
       <label>
         Category
         <select name="category">
           {/* render <option> elements for each category here */}
+        <select
+          name="category"
+          value={formData.category}
+          onChange={handleOnChange}
+        >
+          {optionsList}
+        </select>
         </select>
       </label>
       <input type="submit" value="Add task" />
+      </form>
     </form>
   );
-}
-
+}}
 export default NewTaskForm;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -6,6 +6,16 @@ function TaskList() {
       {/* display a list of tasks using Task component */}
     </div>
   );
-}
+function TaskList({ tasks, onTaskDelete }) {
+  const taskList = tasks.map((task, index) => (
+    <Task
+      key={index}
+      text={task.text}
+      category={task.category}
+      onTaskDelete={() => onTaskDelete(index)}
+    />
+  ));
+  return <div className="tasks">{taskList}</div>;
+}}
 
 export default TaskList;
